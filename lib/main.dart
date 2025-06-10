c4e064e30c7fd9eee0390b0e2d51d827136e034c
zflyluo <zflyluo@tencent.com>
2025-06-10 20:46:04 +0800

    添加主页

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,201 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tdesign_flutter/tdesign_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  TDThemeData? themeData;
+
+  @override
+  void initState() {
+    initTheme();
+    super.initState();
+  }
+
+  Future<void> initTheme() async {
+    var jsonString = await rootBundle.loadString('assets/theme.json');
+    print('jsonString:$jsonString');
+    TDTheme.needMultiTheme(true);
+    themeData = TDThemeData.fromJson('green', jsonString);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      home: TDTextConfiguration(
+        child: Theme(
+            data: ThemeData(extensions: [themeData!]),
+            child: Builder(
+              builder: (context) {
+                return Scaffold(
+                  appBar: _buildAppBar(context),
+                  // appBar: _buildAppBar(context),
+                  // body: StudyDetail(),
+                  body: body(context),
+                  bottomNavigationBar: _buildBottomTabBar(),
+                );
+              },
+            )),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  Padding body(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+          TDButton(text: "ixanshi ",onTap: (){
+            TDLoadingController.show(context);
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+            TDLoadingController.dismiss();
+          },),
+          // 先显示再加载
+          TDText(
+            '测试文案',
+            textColor: TDTheme.of(context).brandNormalColor,
+            fontFamilyUrl:
+            'https://xinyue.qq.com/m/flutter_web/assets/packages/flutter_component/fonts/FZLanTingHeiS-EB-GB.ttf',
+            fontFamily: FontFamily(fontFamily: 'test'),
+          ),
+          //  // 先加载再显示
+          // child: FutureBuilder(
+          //     future:TDFontLoader.load(name: 'test1', fontFamilyUrl: 'https://xinyue.qq.com/m/flutter_web/assets/packages/flutter_component/fonts/FZLanTingHeiS-EB-GB.ttf'),
+          //   initialData: false,
+          //   builder: (_,data)=>TDText(
+          //     (data.data ?? false) ? '测试文案' : '',
+          //     textColor: TDTheme.of(context).brandNormalColor,
+          //     fontFamilyUrl: 'https://xinyue.qq.com/m/flutter_web/assets/packages/flutter_component/fonts/FZLanTingHeiS-EB-GB.ttf',
+          //     fontFamily: FontFamily(fontFamily: 'test1'),
+          //   ),
+          // ),
+          TDInput(
+            // leftLabel: '标签文字',
+            // controller: controller[0],
+            type: TDInputType.cardStyle,
+            backgroundColor: Colors.white,
+            cardStyle: TDCardStyle.topTextWithBlueBorder,
+            hintText: '请输入文字',
+            cardStyleTopText: '标签文字',
+            // onChanged: (text) {
+            //   setState(() {});
+            // },
+            // onClearTap: () {
+            //   controller[0].clear();
+            //   setState(() {});
+            // },
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const TDTextarea(
+            label: '标签文字',
+            hintText: '请输入文字',
+            maxLines: 4,
+            minLines: 4,
+            maxLength: 500,
+            padding: EdgeInsets.zero,
+            indicator: true,
+            // backgroundColor: Colors.white,
+            // textInputBackgroundColor: Colors.white,
+            layout: TDTextareaLayout.vertical,
+            bordered: true,
+          )
+        ],
+      ),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+  PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return TDNavBar(
+        useDefaultBack: false,
+        // screenAdaptation: false,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    Colors.red, Colors.green
+                  ]
+              )
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        // opacity: 0,
+        backgroundColor: Colors.red,
+        centerTitle: false,
+        titleMargin: 0,
+        titleWidget: TDSearchBar(
+          needCancel: false,
+          autoHeight: true,
+          backgroundColor: Colors.transparent,
+          padding: const EdgeInsets.fromLTRB(0, 2, 0, 2),
+          placeHolder: '搜索预设文案',
+          mediumStyle: true,
+          style: TDSearchStyle.round,
+          onTextChanged: (String text) {
+            print('input：$text');
+          },
+        ),
+        rightBarItems: [
+          TDNavBarItem(icon: TDIcons.home, iconSize: 24),
+          TDNavBarItem(icon: TDIcons.ellipsis, iconSize: 24)
+        ]);
+  }
+
+  TDBottomTabBar _buildBottomTabBar() {
+    var iconSize = 39 * 60 / 98;
+    var textSize = 8.0;
+    return TDBottomTabBar(
+      TDBottomTabBarBasicType.iconText,
+      componentType: TDBottomTabBarComponentType.normal,
+      useVerticalDivider: false,
+      barHeight: 98 * 60 / 98,
+      navigationTabs: [
+        TDBottomTabBarTabConfig(
+          selectedIcon: Icon(TDIcons.home, size: iconSize, color: Colors.red),
+          unselectedIcon: Icon(TDIcons.home, size: iconSize, color: const Color(0xFF383838)),
+          tabText: '首页',
+          selectTabTextStyle: TextStyle(fontSize: textSize, color: Colors.red),
+          unselectTabTextStyle: TextStyle(fontSize: textSize, color: Colors.black),
+          onTap: () {
+            // context.read<CurrentIndexProvider>().changeIndex(0);
+          },
+        ),
+        TDBottomTabBarTabConfig(
+          selectedIcon: Icon(TDIcons.app, size: iconSize, color: Colors.red),
+          unselectedIcon: Icon(TDIcons.app, size: iconSize, color: const Color(0xFF383838)),
+          tabText: '办事',
+          selectTabTextStyle: TextStyle(fontSize: textSize, color: Colors.red),
+          unselectTabTextStyle: TextStyle(fontSize: textSize, color: Colors.black),
+          onTap: () {
+            // context.read<CurrentIndexProvider>().changeIndex(1);
+          },
+        ),
+        TDBottomTabBarTabConfig(
+          selectedIcon: Icon(TDIcons.user, size: iconSize, color: Colors.red),
+          unselectedIcon: Icon(TDIcons.user, size: iconSize, color: Color(0xFF383838)),
+          tabText: '我的',
+          selectTabTextStyle: TextStyle(fontSize: textSize, color: Colors.red),
+          unselectTabTextStyle: TextStyle(fontSize: textSize, color: Colors.black),
+          onTap: () {
+            // context.read<CurrentIndexProvider>().changeIndex(2);
+          },
+        ),
+      ],
     );
   }
 }
+
